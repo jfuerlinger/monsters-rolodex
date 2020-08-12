01790f3be0a50c354193c9cd93451414a1597a88
Josef Fürlinger <josef.fuerlinger@gmail.com>
2020-08-12 15:41:24 +0200

    integrate react monsters sample

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+import { CardList } from './components/card-list/card-list.component'
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+
+    return (
+      <div className="App">
+        <input
+          type='text'
+          placeholder='search monsters'
+          onChange={e => this.setState({ searchField: e.target.value })
+          } />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
