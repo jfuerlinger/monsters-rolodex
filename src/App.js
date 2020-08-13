8ce97b9a36c62e8eda2e480c88269e2a3eede2f8
Josef Fürlinger <josef.fuerlinger@gmail.com>
2020-08-13 09:37:06 +0200

    add api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { CardList } from './components/card-list/card-list.component'
+import { Message } from './components/message/message.component'
 
 class App extends Component {
 
@@ -10,24 +11,31 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: '',
+      message: ''
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
+
+    fetch(`/api/message`)
+      .then(response => response.json())
+      .then(content => this.setState({ message: content.text }) )
   }
 
   render() {
 
-    const { monsters, searchField } = this.state;
+    const { monsters, searchField, message } = this.state;
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
 
 
     return (
       <div className="App">
+        <Message message={message} />
         <input
           type='text'
           placeholder='search monsters'
---
+++ src/components/message/message.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './message.styles.css';
+
+export const Message = (props) => (
+    <p> {props.message}</p>
+)
